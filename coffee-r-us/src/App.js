36b68126649898e30f5ad0d31474d99db669581b
Calvin Suiter <cdsuiter@yahoo.com>
2025-07-19 16:55:02 -0400

    Inital commit

---
+++ coffee-r-us/src/App.js
@@ -0,0 +1,8 @@
+<BrowserRouter>
+  <Header />
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/shop" element={<Shop />} />
+    <Route path="/admin" element={<AdminPortal />} />
+  </Routes>
+</BrowserRouter>
